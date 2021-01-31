8e8fee98bde9450997a34ef91708f7489aeb8d4d
singhaniaayush1010@gmail.com <singhaniaayush1010@gmail.com>
2021-01-31 14:09:26 +0530

    routes added

--- server/routers/Router.go
+++ server/routers/Router.go
@@ -2,6 +2,7 @@ package routers
 
 import (
 	"net/http"
+	"todo/server/controller"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,5 +13,10 @@ func Init(route *gin.Engine) {
 	route.GET("/", func(contect *gin.Context) {
 		contect.JSON(http.StatusOK, gin.H{"data": "Welcome to our Todo Application"})
 	})
+	route.GET("/todo", controller.RetrieveAllTodos)
+	route.GET("/todo/:id", controller.RetrieveTodoByID)
+	route.DELETE("/todo/:id", controller.DeleteTodoByID)
+	route.POST("/todo", controller.SaveTodo)
+	route.PUT("/todo", controller.UpdateTodo)
 
 }
